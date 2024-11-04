543c50e37451659eb9b7d4ddfddcdd249f6b286d
Étienne Breton <skylerauraarena@gmail.com>
2024-11-04 21:50:19 +0100

    implémentation du script 1 de préprocessing

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Rediriger toutes les sorties vers setup.log et afficher à l'écran
+exec > >(tee -a setup.log) 2>&1
+
+# Variables
+FEATURES_DIR="$(pwd)/src/features"        # Utiliser un chemin absolu pour éviter les ambiguïtés
+
+python $FEATURES_DIR/preprocessing.py
+
+
+echo "Traitement terminé avec succès."
+
+#______________________________________________________________________________________________________________________________
